18268c3101bac4217e285f459b384bde697105e3
Erik Tews <erik@datenzone.de>
2015-08-29 16:49:06 +0200

    New script based on python cryptography instead of OpenSSL.
    Also support Subject Alternative Name.

--- get_cn.py
+++ get_cn.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 
 import argparse
-from OpenSSL import crypto
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.x509.oid import ExtensionOID, NameOID
 
 def get_subject(f):
-	""" extract the common name from a certificate
+	""" extract the common names (subject and subject alternative name
+	from a certificate 
 	"""
-	c = crypto.load_certificate(crypto.FILETYPE_ASN1, f.read())
-	s = c.get_subject()
-	for [a,b] in s.get_components():
-		if (a == 'CN'):
-			if not " " in b:
-				return b
-	return ""
+	c = x509.load_der_x509_certificate(f.read(),default_backend()) 
+	res = []
+	cn = c.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+	for i in cn:
+		t = i.value
+		# Common name is sometimes not a hostname, remove all that
+		# contain a whitespace
+		if (" " not in t):
+			res.append(t) 
+	try:
+		# Get the Subject Alternative Name extension
+		ext = c.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+		return res + ext.value.get_values_for_type(x509.DNSName)
+	except:
+		pass 
+	return res
+
 
 def main():
         parser = argparse.ArgumentParser(description='print the cn of a certificate')
-	parser.add_argument('f', type=file, nargs="+", metavar='filename', help='DER encoded certificate file')
+	parser.add_argument('f', type=file, nargs="+", metavar='filename', help='DER encoded certificate file(s)')
 	args = parser.parse_args()
 	for i in args.f:
 		try:
-			print get_subject(i)
+			for i in get_subject(i):
+				print i
 		except:
 			pass
 
